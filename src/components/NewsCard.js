55925f7608cbc25ac3184be092fd23dda1084df4
zcabrgb <radu.filimon.16@ucl.ac.uk>
2019-08-18 19:36:11 +0100

    added news card stateless component

---
+++ src/components/NewsCard.js
@@ -0,0 +1,25 @@
+const NewsCard = props => {
+  return (
+    <div class="blog-card">
+      <div class="meta">
+        <div
+          class="photo"
+          style="background-image: url(https://storage.googleapis.com/chydlx/codepen/blog-cards/image-1.jpg)"
+        />
+        <ul class="details">
+          <li class="author">
+            <a href="#">{props.author}</a>
+          </li>
+          <li class="date">{props.date}</li>
+        </ul>
+      </div>
+      <div class="description">
+        <h>{props.title}</h>
+        <p> {props.description}</p>
+        <p class="read-more">
+          <a href="#">Read More</a>
+        </p>
+      </div>
+    </div>
+  );
+};
