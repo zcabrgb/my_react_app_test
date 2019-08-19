233da47bd156f91bcda63a25f4ce394023f268d6
zcabrgb <radu.filimon.16@ucl.ac.uk>
2019-08-19 13:28:49 +0100

    added unit tests for NewsCard

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,35 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import NewsCard from "./components/NewsCard";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
+var createComponent = require("react-unit");
+
+it("renders without crashing", () => {
+  const div = document.createElement("div");
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+
+describe("News Card component", () => {
+  var component = createComponent.shallow(
+    <NewsCard
+      picture={"picture"}
+      title={"title"}
+      description={"txt"}
+      url={"url"}
+    />
+  );
+
+  it("correct picture source", () => {
+    var input = component.findByQuery("img")[0];
+
+    expect(input.props.src).toBe("picture");
+  });
+
+  it("correct behaviour when pic src is missing", () => {
+    var input = component.findByQuery("img")[0];
+
+    expect(input.props.alt).toBe("Picture not available");
+  });
 });
